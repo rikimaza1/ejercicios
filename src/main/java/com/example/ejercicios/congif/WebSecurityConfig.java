fe01bb41a11449a066992d2405858ba1bbc9347f
Riki Maza <riki_30797@hotmail.es>
2022-11-29 13:55:18 +0100

    spring heroku app config

--- src/main/java/com/example/ejercicios/congif/WebSecurityConfig.java
+++ src/main/java/com/example/ejercicios/congif/WebSecurityConfig.java
@@ -19,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/hello").permitAll()
+                .antMatchers("/api/hello").hasRole("USER")
                 .antMatchers("/bootstrap").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
